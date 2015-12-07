895461d8d881a137017dff6af3deab494b0ed76b
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-07 22:42:52 +0000

    Too hasty again.

--- 02/gift_wrap.rb
+++ 02/gift_wrap.rb
@@ -7,15 +7,14 @@ puts "input:#{input}"
 
 total_paper = 0
 input.split("\n").each do |gift|
-  smallest = 9001
-  gift_paper = 0
-  gift.split('x').each do |dim|
-    dim = dim.to_i
-    if dim < smallest; smallest = dim end  # Update smallest dim if needed.
-    if (gift_paper != 0); gift *= dim;
-    else gift_paper += dim; end
-  end
-  gift_paper += smallest  # Add the smallest dimension for slack.
+  gift = gift.split('x')
+  l = gift[0].to_i
+  w = gift[1].to_i
+  h = gift[2].to_i
+  smallest = [l,w,h].min
+
+  gift_paper = 2*l*w + 2*w*h + 2*h*l + smallest
+
   total_paper += gift_paper
 end
 puts "The elves need #{total_paper} square feet of wrapping paper."
