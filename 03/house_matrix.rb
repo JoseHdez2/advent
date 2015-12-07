6fcb2ba5629cdebf0ac222adcd401e9c3ab2f6d8
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-07 23:55:43 +0000

    Incorrect answer.

--- 03/house_matrix.rb
+++ 03/house_matrix.rb
@@ -5,17 +5,21 @@ Dir.chdir(Dir.getwd.split("/advent")[0].concat("/advent/03"))
 input = File.open("./input.txt", "rb").read
 puts "input:#{input}"
 
-x = 0; y = 0
+sanPos = [0,0]; robPos = [0,0]
 visited = Hash.new(0)
-visited[[0,0]] += 1
+visited[[0,0]] += 2
+turn = :santa
 input.split('').each do |move|
+  turn = (turn == :santa)? :robo : :santa
+  curPos = (turn == :santa)? sanPos : robPos
   case(move)
-  when '^'; y -= 1
-  when 'v'; y += 1
-  when '<'; x -= 1
-  when '>'; x += 1
+  when '^'; curPos[1] -= 1  # y -= 1
+  when 'v'; curPos[1] += 1  # y += 1
+  when '<'; curPos[0] -= 1  # x -= 1
+  when '>'; curPos[0] += 1  # x += 1
   end
-  visited[[x,y]] += 1
+  if turn == :santa then sanPos = curPos else robPos = curPos end
+  visited[curPos] += 1
 end
 
 puts "#{visited.length} houses receive at least one present."
