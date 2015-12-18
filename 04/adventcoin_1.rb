c93a77dee312a01db63a9c002b6a6d8f7009c0a3
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-18 23:29:16 +0000

    Giving up on day 4 4 now (weird Ruby lib stuff)

--- 04/adventcoin_1.rb
+++ 04/adventcoin_1.rb
@@ -7,20 +7,26 @@ puts "input:#{input}"
 
 require 'digest'
 
-# Compute digest by chunks
+def show_md5(input)
+  puts "#{input} -->(MD5)--> #{Digest::MD5.new.hexdigest(input)}"
+end
 
-md5 = Digest::MD5.new
-md5.update 'message1'
-md5 << 'message2'                     # << is an alias for update
+# Example values
+show_md5("abcdef609043")
+show_md5("abcdef609043")
+show_md5("pqrstuv104897") # wrong output and I have no idea why.
 
-
+=begin
 i = 0
 while i < 10000000
   i += 1
-  md5 = Digest::MD5.new
-  md5.update input
-  md5 << i.to_s                    # << is an alias for update
+  md5_in = input + i.to_s
+  md5_out = Digest::MD5.hexdigest (input+i.to_s)
+  # md5 = Digest::MD5.new
+  # md5.update input
+  # md5 << input + i.to_s                    # << is an alias for update
 
-  out =  md5.hexdigest
-  if (out[0..4] == '00000') then puts "good hash using number: #{i}" end
+  # out =  md5.hexdigest
+  if (md5_out[0..4] == '00000') then puts "good hash using number: #{i}\n\t #{md5_in} gives #{md5_out}" end
 end
+=end
