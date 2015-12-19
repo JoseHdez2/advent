40f4e3044821d79309900ceb81eacc2d6c0e7341
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-19 00:08:52 +0000

    day 5 part 1 solution.

--- 05/nice_string_1.rb
+++ 05/nice_string_1.rb
@@ -6,27 +6,18 @@ input = File.open("./input.txt", "rb").read
 # puts "input:#{input}"
 
 def is_nice(string)
-  puts 'no 3 vow'; return false unless /.*[aeiou].*[aeiou].*[aeiou].*/ =~ string # at least 3 vowels
-  puts 'no twice'; return false unless /(\w)\1/ =~ string # letter that appears twice in a row
-  puts 'has ugly'; return false unless /ab|cd|pq|xy/ =~ string # no ab, cd, pq, xy.
+  return false unless /.*[aeiou].*[aeiou].*[aeiou].*/ =~ string # at least 3 vowels
+  return false unless /(\w)\1/ =~ string # letter that appears twice in a row
+  return false if /(ab|cd|pq|xy)/ =~ string # no ab, cd, pq, xy.
   return true
 end
 
-examples = ["ugknbfddgicrmopn","jchzalrnumimnmhp","haegwjzuvuyypxyu","dvszwmarrgswjxmb"]
-examples.each do |ex|
-  puts "#{ex} is nice: #{is_nice(ex)}"
-end
-
-=begin
 nice_strings = 0
 input.each_line do |str|
-  if /[aeiou]{3,}/ =~ str and # at least 3 vowels
-    /(\w)\1/ =~ str and # letter that appears twice in a row
-    not /ab|cd|pq|xy/ =~ str # no ab, cd, pq, xy.
+  if is_nice(str)
     puts str
     nice_strings += 1
   end
 end
 
 puts "\n#{nice_strings} strings are nice."
-=end
