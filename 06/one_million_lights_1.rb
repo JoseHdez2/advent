6a0be4a005e3be60d1c63018b0a7c5b32c1740d6
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-19 01:26:39 +0000

    Going to use a multidimensional array.

--- 06/one_million_lights_1.rb
+++ 06/one_million_lights_1.rb
@@ -6,8 +6,7 @@ input = File.open("./input.txt", "rb").read
 
 require 'matrix'
 
-lights = Matrix.zero(1000)
-puts lights
+lights = Hash.new(false)
 
 input.each_line do |order|
   to_do = /^[^\d]+/.match(order)[0].strip.gsub(' ','_').to_s
@@ -15,22 +14,23 @@ input.each_line do |order|
   from = from.split(','); from.each { |d| d = d.to_i }
   to = to.split(','); to.each { |d| d = d.to_i }
 
-  case(to_do)
-  when :turn_on then turn_lights(from, to, true)
-  when :turn_off then turn_lights(from, to, false)
-  when :toggle then turn_lights(from, to, :toggle)
+  val = true if to_do == :turn_on
+  val = false if to_do == :turn_off
+  val = :toggle if to_do == :toggle
+
+  (from[0]..to[0]).each do |x|
+    (from[1]..to[1]).each do |y|
+      lights[[x,y]] = val if val != :toggle
+      lights[[x,y]] = !lights[[x,y]] if val == :toggle
+    end
   end
 end
 
 lit_lights = 0
-x = 0
-while (x < 1000)
-  y = 0
-  while (y < 1000)
-    if lights[x,y] then lit_lights += 1 end
-    y += 1
+(0..999).each do |x|
+  (0..999).each do |y|
+    lit_lights += 1 if lights[[x,y]] == true
   end
-  x += 1
 end
 
 puts "There are #{lit_lights} lit lights."
