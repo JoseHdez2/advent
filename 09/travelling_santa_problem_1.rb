e953685ce545937d6472e28261fe823819a7c9c7
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-20 15:05:24 +0000

    Day 9 part 1 gives wrong answer.

--- 09/travelling_santa_problem_1.rb
+++ 09/travelling_santa_problem_1.rb
@@ -7,11 +7,11 @@ input = File.open("./input.txt", "rb").read
 # ---
 
 # Read distances into hash.
-distances = {}
+distances = Hash.new(Float::INFINITY)
 input.each_line do |line|
   from, to, cost = /(\w+) to (\w+) = (\d+)/.match(line).captures
   # puts "from: #{from}, to: #{to}, cost: #{cost}"
-  distances[[from, to]] = cost
+  distances[[from, to]] = cost.to_i
 end
 
 # Induce unique locations
@@ -19,3 +19,18 @@ locations = []
 distances.keys.each { |k| k.each { |l| locations.push(l) } }
 locations.uniq!
 puts locations
+
+min_tour_cost = Float::INFINITY
+# Try all possible permutations of locations
+locations.permutation do |tour|
+  tour_cost = 0
+  (1...tour.length).each do |i|
+    from = tour[i-1]; to = tour[i]
+    tour_cost += distances[[from,to]]
+  end
+  puts "tour: #{tour}, cost: #{tour_cost}" if tour_cost < Float::INFINITY
+
+   min_tour_cost = tour_cost if (tour_cost < min_tour_cost)
+end
+
+puts "Cost of shortest tour is #{min_tour_cost}."
