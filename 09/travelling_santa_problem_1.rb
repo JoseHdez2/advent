df2648709feac1cceaa8c4b610c083911e676a77
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-20 16:11:09 +0000

    Needed Dijkstra for implicit dists. Almost

--- 09/travelling_santa_problem_1.rb
+++ 09/travelling_santa_problem_1.rb
@@ -2,23 +2,64 @@
 Dir.chdir(Dir.getwd.split("/advent")[0].concat("/advent/09"))
 
 # Read input
-input = File.open("./input.txt", "rb").read
+input = File.open("./example.txt", "rb").read
 
 # ---
 
-# Read distances into hash.
-distances = Hash.new(Float::INFINITY)
+require 'set'
+
+# Read explicit distances into hash.
+map = Hash.new(Float::INFINITY)
 input.each_line do |line|
   from, to, cost = /(\w+) to (\w+) = (\d+)/.match(line).captures
   # puts "from: #{from}, to: #{to}, cost: #{cost}"
-  distances[[from, to]] = cost.to_i
+  map[[from, to]] = cost.to_i
 end
 
 # Induce unique locations
-locations = []
-distances.keys.each { |k| k.each { |l| locations.push(l) } }
-locations.uniq!
+locations = Set.new []
+map.keys.each { |k| k.each { |l| locations.add(l) } }
 puts locations
+
+# Distances hash, to contain explicit and implicit distances
+distances = map
+
+# Distance of a node to itself is 0 (at least in this problem).
+locations.each { |l| distances[[l,l]] = 0 }
+
+# Given a node, return its neighboring nodes
+# For Dijkstra algorithm coming up
+def neighbors(node_name, dists)
+  neighs = []
+  dists.keys.each do |k|
+    neighs.push(k) if k.contains(node_name)
+  end
+  return neighs.flatten.delete(node_name)
+end
+
+# Get implicit and update explicit distances using Dijkstra
+locations.each do |start_node|
+  locations.each do |end_node|
+    unvisited = locations
+    while(not unvisited.empty?)
+      # Evaluated node will be closest unvisited node
+      closest_unvisited = nil
+      unvisited.each do |uv|
+        if distances[[start_node,uv]] < distances[[start_node,closest_unvisited]]
+        then closest_unvisited = uv end
+      end
+      cur_node = closest_unvisited
+      neighbors(cur_node, map).each do |n|
+        comparison = [distances[start_node,n]] # Old distance
+        comparison.push(distances[start_node,cur_node]+distances[cur_node,n]) # New distance
+        distances[start_node,n] = comparison.min
+      end
+      unvisited.delete(cur_node)
+    end
+  end
+end
+
+# Coulda used the matrix algorithm but w/e.
 
 min_tour_cost = Float::INFINITY
 # Try all possible permutations of locations
