66b211af4705aa41ac6dfcec10da3c261c3f9d4a
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-21 11:53:36 +0000

    Was using same file. Still doesn't work.
    Somehow output is not a file but an empty string.

--- 10/look_and_say_2.rb
+++ 10/look_and_say_2.rb
@@ -3,14 +3,14 @@ Dir.chdir(Dir.getwd.split("/advent")[0].concat("/advent/10"))
 
 # Read input
 input = File.open("./input.txt", "rb").read
-output = File.open("./input.txt", "rb").read
+output = File.open("./output.txt", "a+").read
 
 # ---
-input.strip!; output.strip!
+input.strip!; #output.strip!
 
 # R.I.P. CPU
 (0...50).each do |i|
-  if output.length >= i then input = output[i]; next # Skip already-calculated values.
+  if output.lines.length >= i+1 then input = output.lines[i]; puts "read line #{i}"; next end # Skip already-calculated values.
 
   puts i # puts input
   jackpot = [[input[0]]]
@@ -23,6 +23,7 @@ input.strip!; output.strip!
   jackpot.each do |cluster|
     input += cluster.length.to_s + cluster[0]
   end
+  output.write(input)
 end
 
 # puts input
