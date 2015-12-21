11d2b072da674379fc48d5404f43c0aea5f9d8f3
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-21 01:03:25 +0000

    I was taking the input and recreating it! so dumb

---
+++ 10/look_and_say_1.rb
@@ -0,0 +1,25 @@
+# Go into folder
+Dir.chdir(Dir.getwd.split("/advent")[0].concat("/advent/10"))
+
+# Read input
+input = File.open("./input.txt", "rb").read
+
+# ---
+input.strip!
+
+(0...40).each do
+  puts input
+  jackpot = [input[0]]
+  input[1..input.size].split('').each do |d|
+    if d == jackpot[-1][0] then jackpot[-1].push(d)
+    else jackpot.push([d]) end
+  end
+
+  input = ''
+  jackpot.each do |cluster|
+    puts "num= #{cluster[0]} times =#{cluster.length}"
+    input += cluster[0] * cluster.length
+  end
+end
+
+puts input; puts input.length
