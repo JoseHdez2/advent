930a11b1396f766639e05f7551f7bcae10297c44
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-30 20:44:36 +0000

    Doing day 6 part 1.
    Going to get some OOP up in here.

--- 06/one_million_lights_1.rb
+++ 06/one_million_lights_1.rb
@@ -2,35 +2,48 @@
 Dir.chdir(Dir.getwd.split("/advent")[0].concat("/advent/06"))
 
 # Read input
-input = File.open("./input.txt", "rb").read
+# input = File.open("./input.txt", "rb").read
+input = File.open("./dummy.txt", "rb").read
 
-lights = Array.new(1000) { Array.new(1000,0) }
+# 1000x1000 matrix of integers
+# lights = Array.new(1000) { Array.new(1000,0) }
+lights = Array.new(8) { Array.new(8,0) }
+
+# puts lights
 
 input.each_line do |order|
-  to_do = /^[^\d]+/.match(order)[0].strip.gsub(' ','_').to_sym
+  to_do = /^[^\d]+/.match(order)[0].strip
   from, to = /(\d+,\d+).*?(\d+,\d+)/.match(order).captures
-  from = from.split(','); from.each { |d| d = d.to_i }
-  to = to.split(','); to.each { |d| d = d.to_i }
+  from = from.split(',').map { |e| e.to_i }
+  to = to.split(',').map { |e| e.to_i }
 
-  val = 1 if to_do == :turn_on
-  val = 0 if to_do == :turn_off
-  val = :toggle if to_do == :toggle
+  p "from:#{from} to:#{to} to_do:#{to_do}"
 
   (from[0]..to[0]).each do |x|
     (from[1]..to[1]).each do |y|
-      lights[x][y] = val if val != :toggle
-      lights[x][y] = !lights[x][y] if val == :toggle
+      lights[x][y] = 0 if to_do == "turn off"
+      lights[x][y] = 1 if to_do == "turn on"
+
+      # Toggle if instruction is "toggle"
+      if to_do == "toggle"
+        lights[x][y] = 0 if lights[x][y] == 1
+        lights[x][y] = 1 if lights[x][y] == 0
+      end
     end
   end
+
+  puts lights
 end
 
 lit_lights = 0
+# str = ""
 (0..999).each do |x|
   (0..999).each do |y|
     lit_lights += 1 if lights[x][y] == 1
-    if lights[x][y] then p 'o ' else p '. ' end
+    # if lights[x][y] then str += 'o ' else str += '. ' end
   end
-  puts
+  # str += "\n"
 end
+# puts str
 
 puts "There are #{lit_lights} lit lights."
